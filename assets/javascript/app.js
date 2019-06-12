7fd9612532fda2c6124706f00692b460377ab55c
Cohen <dcoco91@gmail.com>
2019-06-12 13:46:16 -0400

    Added timer and some code for the countdown timer. added label html tag to radio buttons so text is clickable

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,35 @@
 let correct = 0;
 let wrong = 0;
 let unaswer = 0;
+let time = 30;
+let isRunning = false;
 
-$('#start').on("click", function () {
+
+var timer = function timer() { 
+    
+    if(!isRunning){
+        $('#time-name').text("Time");
+        isRunning = true;
+    }
+    $('#timer').text(time);
+    time--;
+ }
+
+function start(){
     $('#content').removeClass("d-none");
     $('#start').addClass("d-none");
-    
-});
+    let interval = setInterval(timer, 1000);
+    // timer();
+}
 
-$('#submit').on("click", function () {
+function end () {
     $('#content').remove();
-    
-});
+}
+
+ window.onload = function (){
+    $('#start').on("click", start);
+    $('#submit').on("click", end);
+ }
+
+
+
